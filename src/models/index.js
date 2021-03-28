ff8d7612ed8d2c431c9cc1a807cdf6b2b9649128
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2021-03-28 22:51:23 +0100

    model auth updated

--- src/models/index.js
+++ src/models/index.js
@@ -18,15 +18,15 @@ mongoose.connect(env.databaseURL,
   });
 const db = mongoose.connection;
 
-const databaseSetup = async () => {
-  await db.once('open', () => console.log('connected to database'));
+const databaseSetup = () => {
+  db.once('open', () => console.log('connected to database'));
 };
 
-if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'production') {
-  databaseSetup();
-} else {
+if (process.env.NODE_ENV === 'development') {
   databaseSetup();
   Object.values(models).forEach(async (modelProp) => { await modelProp.deleteMany(); });
+} else {
+  databaseSetup();
 }
 
 export default {
